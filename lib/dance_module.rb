4a17526c1275674c19e1432aae6c7f1044e36bcd
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-18 17:26:06 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,5 +1,5 @@
 module Dance
-  
+
   def twirl
     "I'm twirling!"
   end
@@ -15,5 +15,5 @@ module Dance
   def take_a_bow
     "Thank you, thank you. It was a pleasure to dance for you all."
   end
-  
+
 end
