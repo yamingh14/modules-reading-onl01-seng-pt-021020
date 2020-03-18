2e68df958c1ca74094c6062876a6d1e230c50e66
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-18 17:27:41 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,15 +2,5 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  include Dance
 
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-
-    class Dancer
-      extend MetaDancing
-    end
 end
