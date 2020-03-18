b7f6eb97a50807fd40aecd42018d981d3a0e257c
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-18 17:31:36 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -2,5 +2,12 @@ module MetaDancing
   def metadata
     "This class produces objects that love to dance."
   end
+end
 
+class Dancer
+  extend MetaDancing
+end
+
+class Kid
+  extend MetaDancing
 end
